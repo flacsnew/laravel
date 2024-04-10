2b05bc57178633df375161e2a62e49e6f3e50e33
flacs <vtchislov@pir.company>
2024-04-10 13:27:41 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## Main
+
+#### Подъем проекта в докере (терминал)
+
+```./vendor/bin/sail up```
+
+#### Создание контроллера (в терминал)
+
+```./vendor/bin/sail artisan make:controller MyPageController```
+
+```./vendor/bin/sail artisan make:controller PostController```
+
+
+#### Создание модели в миграции под название Page. -m означает еще и создание файла миграции
+
+```./vendor/bin/sail artisan make:model Page -m```
+
+```./vendor/bin/sail artisan make:model Post -m```
+
+### Миграция (в терминал)
+
+```./vendor/bin/sail artisan migrate```
+
+### Запуск
+
+```http://localhost:8070/```
+
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/my_page',  [App\Http\Controllers\MyPageController::class, 'index']);
+
+Route::get('/posts',  [App\Http\Controllers\PostController::class, 'index']);
+
