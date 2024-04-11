142733afcc9d7c820d94d30d89dfd2c278481749
flacs <vtchislov@pir.company>
2024-04-11 15:31:02 +0300

    firstOrCreate, updateOrCreate functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -55,8 +55,40 @@ class PostController extends Controller
 
     public function delete()
     {
-        //Post::find(2)->delete();
-        Post::withTrashed()->find(2)->restore();
+        Post::find(2)->delete();
+        //Post::withTrashed()->find(2)->restore();
         dd('delete.');
+    }
+
+    public function firstOrCreate()
+    {
+        $expPost = [
+            'title' => "some title of post",
+            'content' => "some content new",
+            'image' => "image6.jpg",
+            'likes' => 400,
+            'is_published' => 1
+        ];
+        $post = Post::firstOrCreate([
+            'title' => 'some title of post'
+        ], $expPost);
+        dump($post->content);
+        dd('up.');
+    }
+
+    public function updateOrCreate()
+    {
+        $expPost = [
+            'title' => "some NEW title of post",
+            'content' => "its not update some content new",
+            'image' => "image7.jpg",
+            'likes' => 800,
+            'is_published' => 1
+        ];
+        $post = Post::updateOrCreate([
+            'title' => 'some title'
+        ], $expPost);
+        dump($post->content);
+        dd('upd.');
     }
 }
