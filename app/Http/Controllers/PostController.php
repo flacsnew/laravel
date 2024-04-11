3c4f9ec28249d00b233f7c2364d404e3382cbd77
flacs <vtchislov@pir.company>
2024-04-11 11:22:14 +0300

    model methods work in DB - Eloquent\Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,37 @@ class PostController extends Controller
 {
     public function index()
     {
-        $post = Post::find(1);
-        dd($post->title);
-        return 'Example Page!';
+        $posts = Post::where('is_published', 1)->get();
+        foreach ($posts as $post)
+        {
+            dump($post->title);
+        }
+        dd('end.');
+    }
+
+    public function create()
+    {
+        $postArr = [
+            [
+                'title' => "title of post",
+                'content' => "some content",
+                'image' => "image4.jpg",
+                'likes' => 20,
+                'is_published' => 1
+            ],
+            [
+                'title' => "another title of post",
+                'content' => "some content post",
+                'image' => "image5.jpg",
+                'likes' => 30,
+                'is_published' => 1
+            ]
+        ];
+
+        foreach ($postArr as $post)
+        {
+            Post::create($post);
+        }
+        dd('created.');
     }
 }
